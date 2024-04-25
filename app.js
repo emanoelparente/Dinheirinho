9bd2fe0e8bb015d6f05c89509fb79989db4ce913
Emanoel Parente <106496107+emanoelparente@users.noreply.github.com>
2024-04-24 21:09:06 -0300

    Atualizações DB

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require ("express");
+const express = require("express");
 const path = require('path');
 const mysql = require("mysql");
 const dotenv = require('dotenv');
@@ -6,34 +6,33 @@ const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser')
 
 
-dotenv.config({path: './.env'})
+dotenv.config({ path: './.env' })
 
 const app = express();
 app.use(cookieParser())
 
 const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE,
-    queryTimeout:60000
+    host: process.env.MYSQL_ADDON_HOST,
+    database: process.env.MYSQL_ADDON_DB,
+    user: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD
 });
 
 const publicDirectory = path.join(__dirname, './public');
 app.use(express.static(publicDirectory));
 
 //Parse URL - encoded bodies (as sent by HTML forms)
-app.use(express.urlencoded({extend:false}));
+app.use(express.urlencoded({ extend: false }));
 //Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
 app.set('view engine', 'hbs');
 
 
-db.connect((error) =>{
-    if(error){
+db.connect((error) => {
+    if (error) {
         console.log(error)
-    }else{
+    } else {
         console.log("MYSQL Connected...")
     }
 })
@@ -46,7 +45,7 @@ app.use('/', require('./routes/pages'));
 app.use('/auth', require('./routes/auth'));
 
 app.listen(3306, () => {
-//app.listen(5000, () => {
+    //app.listen(5000, () => {
     console.log("Servidor iniciado na porta 5000");
 })
 
