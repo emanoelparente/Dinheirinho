bfa8bb1f59024be4787c25f0d1bfe286a7091936
Emanoel Parente <106496107+emanoelparente@users.noreply.github.com>
2024-04-25 15:34:36 -0300

    Update pages.js

--- routes/pages.js
+++ routes/pages.js
@@ -154,7 +154,7 @@ router.post('/recuperaSenha', (req, res) => {
             }
 
             // Após armazenar o token no banco de dados, envie o e-mail
-            const resetPasswordLink = `https://redefinirSenha/${token}`;
+            const resetPasswordLink = `https://dinheirinho.cleverapps.io/redefinirSenha/:${token}`;
             const mailOptions = {
                 from: {
                     name: 'Dinheirinho',
